d3b80aea7e40b0abea066ea610e73ab31dd6be6b
Don Spire <don.spire1@gmail.com>
2021-09-04 21:54:25 -0700

    add routes, connected Atlas

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
-
+  require("dotenv").config();
+}
 
 const express = require("express");
 const app = express();
-// const database = require("./models");
+const database = require("./models");
 const PORT = process.env.PORT || 5000;
 const IP = process.env.IP;
 
-// database.on("error", console.error.bind(console, "Connection ERROR:"));
-// database.once("open", () => {
-//   console.log("Mongo Atlas Database connected");
-// });
+const morgan = require("morgan");
+
+database.on("error", console.error.bind(console, "Connection ERROR:"));
+database.once("open", () => {
+  console.log("Mongo Atlas Database CONNECTED!");
+});
+
+// http request logger, dev setting colorizes output logs
+app.use(morgan("dev"));
 
 // enable bodyParser which is now part of express
-app.use(express.json({ extended: true}));
+app.use(express.json({ extended: true }));
 
-app.get('/', (req, res) => res.json({message: "Welcome to contactKeeper API"}) );
+app.get("/", (req, res) => res.json({ message: "Welcome to Barker4 API" }));
 
-// app.use("/api/users", require("./routes/users"))
-// app.use("/api/auth", require("./routes/auth"))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 // app.use("/api/contacts", require("./routes/contacts"))
 
-app.listen(PORT, () => console.log(`App listening intently on PORT: ${PORT} and IP: ${IP}`));
+app.listen(PORT, () =>
+  console.log(`App listening intently on PORT: ${PORT} and IP: ${IP}`)
+);
