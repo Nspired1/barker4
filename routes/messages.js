a3eb839ccbdc0ba50a9ab410eb8e0943ea3ec2c9
Don Spire <don.spire1@gmail.com>
2021-09-07 13:41:33 -0700

    add messages routes

---
+++ routes/messages.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const Message = require("../models/Message");
+
+// @route   GET api/messages
+// @desc    get all messages
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const messages = await Message.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(messages);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST api/messages
+// @desc    Add new messages
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+
+    [
+      check("text", "Message is required.")
+        .not()
+        .isEmpty()
+        .isLength({ max: 280 }),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { text } = req.body;
+    console.log(text);
+    console.log(req.user.id);
+
+    try {
+      const newMessage = new Message({
+        text,
+        author: req.user.id,
+      });
+
+      const message = await newMessage.save();
+
+      res.json(message);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error.");
+    }
+  }
+);
+
+// @route   PUT api/message/:id
+// @desc    EDIT new messages or could be RETWEET
+// @access  Private
+router.put("/:id", (req, res) => {
+  res.send("EDIT a message");
+});
+
+// @route   DELETE api/message/:id
+// @desc    DELETE new messages
+// @access  Private
+
+router.delete("/:id", auth, async (req, res) => {
+  // auth provides the user and user.id
+  try {
+    const foundMessage = await Message.findById(req.params.id);
+    if (!foundMessage)
+      return res.status(404).json({ msg: "Message not found." });
+    // ensure user owns message
+    if (foundMessage.author.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "That is not authorized" });
+    }
+    await Message.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Message Deleted" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error.");
+  }
+});
+
+module.exports = router;
