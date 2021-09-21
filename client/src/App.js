5cd5feda6a1dccb0ebead3852c86dc8986b1afc6
Don Spire <don.spire1@gmail.com>
2021-09-21 11:44:29 -0700

    fixed bug with displaying pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-// import "./App.css";
+import "./App.css";
+// import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MessageState from "./context/message/MessageState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import Navbar from "./components/layout/Navbar";
+import Landing from "./components/pages/Landing";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
@@ -26,7 +28,8 @@ const App = () => {
             <div className="container">
               <Alerts />
               <Switch>
-                <WithAuth exact path="/" component={Home} />
+                <Route exact path="/" component={Landing} />
+                <WithAuth exact path="/home" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
