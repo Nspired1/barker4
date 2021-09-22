a48f9bf3d3187df7d39432193c0ceab618dae3c8
Don Spire <don.spire1@gmail.com>
2021-09-21 22:10:46 -0700

    image upload components

---
+++ client/src/components/auth/EditUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ImageUpload from "./ImageUpload";
+
+const EditUser = () => {
+  return (
+    <div>
+      <ImageUpload />
+    </div>
+  );
+};
+
+export default EditUser;
