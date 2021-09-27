71a203217c36f9b385a73c460ffc565871d127b3
Don Spire <don.spire1@gmail.com>
2021-09-27 10:53:54 -0700

    add image upload types

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -18,3 +18,6 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+export const IMAGE_UPLOAD = "UPLOAD_IMAGE";
+export const IMAGE_ERROR = "IMAGE_ERROR";
