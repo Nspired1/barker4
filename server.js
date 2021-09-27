38bb67298183252aa86c853db01493c01cec5ff3
Don Spire <don.spire1@gmail.com>
2021-09-27 11:44:42 -0700

    prep to deploy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const database = require("./models");
 const PORT = process.env.PORT || 5000;
 const IP = process.env.IP;
+const path = require("path");
 
 const morgan = require("morgan");
 
@@ -26,6 +27,16 @@ app.get("/", (req, res) => res.json({ message: "Welcome to Barker4 API" }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/messages", require("./routes/messages"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder (that react built)
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // to run server at the command line type: "nodemon" or "node server.js" or "npm start"
 // to run frontend client at the prompt type: "npm run client"
